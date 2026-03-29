d8d1d9b70658128201a4a421363305529e7535ac
unknown <zafirabgr456@gmail.com>
2026-03-29 17:44:19 +0700

    update

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,17 +3,5 @@ import Link from 'next/link'
 import { HomeIcon } from '@/configs/icons'
 
 export default function NotFound() {
-  return (
-    <>
-      <div className="flex flex-col justify-center items-center pt-24">
-        <h1 className="text-3xl mb-6">Page Not Found </h1>
-        <Link href="/" className="flex items-center text-accent">
-          <span className="mr-2">
-            <HomeIcon />
-          </span>
-          Return Home
-        </Link>
-      </div>
-    </>
-  )
+  return <div>404</div>;
 }
